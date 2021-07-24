37de91ae0cde8d9fb8d85d24e997d8ca94bd8abd
Dang Nguyen <dang.nguyen@nfq.asia>
2021-07-24 12:58:13 +0700

    Remove dist from tracking

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export interface UrlOptions {
-    queryParams?: {
-        [x: string]: any;
-    };
-    hash?: string;
-    path?: string;
-    returnAbsoluteUrl?: boolean;
-}
-export default function buildUrl(inputUrl: string | UrlOptions, options?: UrlOptions): string;
